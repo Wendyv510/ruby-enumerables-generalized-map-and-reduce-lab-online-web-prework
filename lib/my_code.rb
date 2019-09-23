3d65c957405be67fb35be8021d88a06f738cabbb
Wendy Harris <wendyv510@hotmail.com>
2019-09-23 00:52:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,40 @@ def my_own_map
   
   do
     dune=["pau","gurney","vladimir", "jessica", "chani"]
+    map(dune){|n|n}
+  end
+  
+  do
+    map[1,2,3,-9]{|n| n*2}
+  end
+  
+  do 
+    map[1,2,3,-9]{|n| n*n}
+  end
+  
+  do
+    source_array=[1,2,3]
+    reduce(source_array){|memo,n| memo+n}
+  end
+  
+  do
+    source_array=[1,2,3]
+    starting_point=100
+    reduce(source_array,starting_point){|memo,n| memo+n}
+  end
+  
+  do 
+    source_array=[1,2,true,"razmatazz"]
+    reduce(source_array){|memo,n| memo && n}
+  end
+  
+  do
+    source_array[falsey,nil,nil,nil,true]
+    reduce (source_array){|memo,n| memo||n}
+  end
+   
+   do 
+     source_array[fasle,nil,nil,nil]
+     reduce(source_array){|memo,n| memo && n}
+   end
+   
